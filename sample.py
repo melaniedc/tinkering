d6129b8444990eb8d8c6593c0c70600241449dd9
Melanie <melanielclements@gmail.com>
2021-05-11 14:16:18 -0400

    Create sample.py
    Add a sample file in python.

---
+++ sample.py
@@ -0,0 +1,8 @@
+####  Copyright 2021 Google LLC.
+####  SPDX-License-Identifier: Apache-2.0
+
+my_string_variable = "Hello World!"
+my_int_variable = 3
+
+for i in range(my_int_variable):
+  print my_string_variable
